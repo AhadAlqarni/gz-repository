e842263e17a6bac69b5a3e9530c67159e4ce70fb
Ahad Alqarni <ahad.qsaad@gmail.com>
2023-10-18 11:17:42 +0000

    commit make macros challenge 6

--- models/sales.sql
+++ models/sales.sql
@@ -21,5 +21,6 @@ SELECT
 	,ROUND(s.quantity*CAST(p.purchSE_PRICE AS FLOAT64),2) AS purchase_cost
 	-- margin --
 	,ROUND(s.revenue-s.quantity*CAST(p.purchSE_PRICE AS FLOAT64),2) AS margin
+    ,{{ margin('s.revenue', 's.quantity*CAST(p.purchSE_PRICE AS FLOAT64)') }} AS product_margin_percent
 FROM sales s
 INNER JOIN product p ON s.pdt_id = p.products_id
